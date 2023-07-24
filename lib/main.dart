e00af47805ed4ede7d8b26914295ae4b4b4d1639
parkjs <python2_7@naver.com>
2023-07-24 23:08:10 +0900

    goRouter init

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+      path: '/setting',
+      builder: (context, state) => SettingScreen(),
+    ),
+  ],
+);
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routerConfig: _router,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar('HomePage'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              '홈페이지',
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                context.push('/setting');
+              },
+              child: Text('설정화면'))
+        ],
+      ),
+    );
+  }
+}
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar('SettingPage'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text('설정화면'),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text('돌아가기'))
+        ],
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String _title;
+  MyAppBar(this._title);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(_title),
+      backgroundColor: Colors.amber,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(56.0);
+}
